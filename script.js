81aaef79190b0940d3779236127ab6ea90db2003
Irving Bautista <irvingbautista681@gmail.com>
2025-06-25 13:39:26 -0700

    feat: add playRound() function to play one round of rock paper scissors

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("hello World");
 
+let humanScore = 0, computerScore = 0;
+
+
 function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
     
@@ -8,12 +11,63 @@ function getComputerChoice () {
     } else if (choice === 1) {
         return "paper";
     } else {
-        return "scissor";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
     let input = prompt("Rock, Paper, or Scissors?");
+    return input;
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("Tie!")
+        return;
+    }
+
+    switch (humanChoice) {
+        case "rock":
+          if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+          } else {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+          }
+          break;
+    
+        case "paper":
+          if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+          } else {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore++;
+          }
+          break;
+    
+        case "scissors":
+          if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+          } else {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+          }
+          break;
+    
+        default:
+          console.log("Invalid choice. Please choose rock, paper, or scissors.");
+      }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Human chooses " + humanSelection);
+console.log("Computer chooses " + computerSelection);
+
+playRound(humanSelection, computerSelection);
